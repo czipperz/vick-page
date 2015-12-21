591ddb2ec2a4630c974dc17c7cb5d84defae4d19
Czipperz <czipperz@gmail.com>
2015-12-21 11:35:00 -0600

    Init

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
-# My plugin [![Build Status](https://travis-ci.org/czipperz/vick-plugin-base.svg?branch=master)](https://travis-ci.org/czipperz/vick-plugin-base)
+# vick-page [![Build Status](https://travis-ci.org/czipperz/vick-page.svg?branch=master)](https://travis-ci.org/czipperz/vick-page)
 
-What it does, how to use in `configuration.cc`
+Allows for movement on the buffer relative to the page
+(`contents::y_offset` and `contents::max_y`).
+
+Defines all functions in the `namespace page`.
+
+| name                          | default binding |
+|-------------------------------|-----------------|
+| `page::move_to_window_top`    | `zt`            |
+| `page::move_to_window_middle` | `zm`            |
+| `page::move_to_window_bottom` | `zb`            |
+| `page::move_to_window_top`    | `zT`            |
+| `page::move_to_window_middle` | `zM`            |
+| `page::move_to_window_bottom` | `zB`            |
+| `page::move_up_page`          | `^B`            |
+| `page::move_down_page`        | `^F`            |
+| `page::move_up_half_page`     | `^U`            |
+| `page::move_down_half_page`   | `^D`            |
---
+++ src/page.hh
@@ -0,0 +1,134 @@
+#ifndef HEADER_GUARD_PAGE_H
+#define HEADER_GUARD_PAGE_H
+
+#include "../../../src/contents.hh"
+
+/*!
+ * \file page.hh
+ *
+ * \brief Defines functions for moving up and down using
+ * `contents.max_y` as the size of the page.
+ */
+
+namespace vick {
+namespace page {
+
+/*!
+ * \brief Moves down half of `contents.max_y` and bounds checks.
+ *
+ * `contents.x` is unchanged.
+ *
+ * \post contents.y < contents.cont.size()
+ */
+boost::optional<std::shared_ptr<change> >
+move_down_half_page(contents&, boost::optional<int> = 1);
+
+/*!
+ * \brief Moves up half of `contents.max_y` and bounds checks.
+ *
+ * `contents.x` is unchanged.
+ *
+ * \post contents.y >= 0
+ */
+boost::optional<std::shared_ptr<change> >
+move_up_half_page(contents&, boost::optional<int> = 1);
+
+/*!
+ * \brief Moves down `contents.max_y` and bounds checks.
+ *
+ * `contents.x` is unchanged.
+ *
+ * \post contents.y < contents.cont.size()
+ */
+boost::optional<std::shared_ptr<change> >
+move_down_page(contents&, boost::optional<int> = 1);
+
+/*!
+ * \brief Moves up `contents.max_y` and bounds checks.
+ *
+ * `contents.x` is unchanged.
+ *
+ * \post contents.y >= 0
+ */
+boost::optional<std::shared_ptr<change> >
+move_up_page(contents&, boost::optional<int> = 1);
+
+/*!
+ * \brief Moves `contents.y_offset` such that it is equal to
+ * `contents.y`.
+ *
+ * `contents.x` and `contents.y` is unchanged.
+ *
+ * \post `contents.y = contents.y_offset` (but `contents.y` is
+ *       unchanged.)
+ * \post `contents.y > 0`
+ */
+boost::optional<std::shared_ptr<change> >
+recenter_top(contents&, boost::optional<int> = boost::none);
+
+/*!
+ * \brief Moves `contents.y_offset` such that `contents.y` is centered
+ * between `contents.y_offset` and `contents.y_offset +
+ * contents.max_y`.
+ *
+ * `contents.x` and `contents.y` is unchanged.
+ *
+ * \post `contents.y = contents.y_offset + contents.max_y / 2 - 1`
+ *       (but `contents.y` is unchanged.)
+ * \post `contents.y > 0`
+ */
+boost::optional<std::shared_ptr<change> >
+recenter_middle(contents&, boost::optional<int> = boost::none);
+
+/*!
+ * \brief Moves `contents.y_offset` such that `contents.y` is equal to
+ * `contents.y_offset + contents.max_y`.
+ *
+ * `contents.x` and `contents.y` is unchanged.
+ *
+ * \post `contents.y = contents.y_offset + contents.max_y - 2` (but
+ *       `contents.y` is unchanged.)
+ * \post `contents.y > 0`
+ */
+boost::optional<std::shared_ptr<change> >
+recenter_bottom(contents&, boost::optional<int> = boost::none);
+
+/*!
+ * \brief Moves `contents.y` such that it is equal to
+ * `contents.y_offset`
+ *
+ * `contents.x` and `contents.y_offset` is unchanged.
+ *
+ * \post `contents.y = contents.y_offset` (simple assignment.)
+ */
+boost::optional<std::shared_ptr<change> >
+move_to_window_top(contents&, boost::optional<int> = boost::none);
+
+/*!
+ * \brief Moves `contents.y` such that it is centered between
+ * `contents.y_offset` and `contents.y_offset + contents.max_y`
+ *
+ * `contents.x` and `contents.y_offset` is unchanged.
+ *
+ * \post `contents.y = contents.y_offset + contents.max_y / 2 - 1`
+ *       (simple assignment.)
+ */
+boost::optional<std::shared_ptr<change> >
+move_to_window_middle(contents&, boost::optional<int> = boost::none);
+
+/*!
+ * \brief Moves `contents.y` such that it is equal to
+ * `contents.y_offset + contents.max_y - 2`
+ *
+ * `contents.x` and `contents.y_offset` is unchanged.
+ *
+ * \post `contents.y = contents.y_offset + contents.max_y` (simple
+ *       assignment.)
+ */
+boost::optional<std::shared_ptr<change> >
+move_to_window_bottom(contents&, boost::optional<int> = boost::none);
+
+}
+}
+
+#endif
